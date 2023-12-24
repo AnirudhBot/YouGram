2abc02d322459bcd16ff1f6de6806d82e3f5f126
Anirudh Sharma <anirudh1304@gmail.com>
2023-12-24 23:11:17 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGODB)
+    .then(() => {
+      console.log("DB connected");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.listen(8800, () => {
+  connect();
+  console.log("server running");
+});
